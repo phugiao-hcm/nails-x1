946bf016ce4920f70ebc3ee260b520d4ed29c8d6
Thanh Nguyen <thanh.nguyen@go2joy.vn>
2025-07-31 16:17:02 +0700

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,11 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-    // ssr: false, // vì GitHub Pages chỉ hỗ trợ static
-    // target: "static", // không bắt buộc nhưng rõ ràng
-    // app: {
-    //     baseURL: "/nails-x1/", // ⚠️ thay <REPO_NAME> bằng tên repo của bạn
-    // },
+    ssr: false, // vì GitHub Pages chỉ hỗ trợ static
+    target: "static", // không bắt buộc nhưng rõ ràng
+    app: {
+        baseURL: "/nails-x1/", // ⚠️ thay <REPO_NAME> bằng tên repo của bạn
+    },
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
     css: ["~/assets/css/main.css"],
